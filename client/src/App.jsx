9a0d03338b41462e6ea8cbd055281e7b76f96ca7
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-08-25 10:32:17 +0530

    Implement routing and header for blog application; add Home and Add Blog pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header/index.jsx";
+import Home from "./pages/home/index.jsx";
+import AddNewBlog from "./pages/add-blog/index.jsx";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/add-blog" element={<AddNewBlog />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+import classes from "./styles.module.css";
+
 export default function Header() {
   return (
-    <div>
+    <div className={classes.header}>
       <h3>Mer Blog App</h3>
       <ul>
-        <li>Home</li>
-        <li>Add Blog</li>
+        <Link to={"/"}>
+          <li>Home</li>
+        </Link>
+        <Link to={"/add-blog"}>
+          <li>Add Blog</li>
+        </Link>
       </ul>
     </div>
   );
